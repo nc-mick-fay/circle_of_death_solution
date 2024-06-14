031137497550baf7207aaf1642f69cf32ab06943
nc-mick-fay <michael.fay@northcoders.com>
2024-06-14 13:20:03 +0100

    Refactor solution to circle_iterator

--- src/circle_iterator.py
+++ src/circle_iterator.py
@@ -9,31 +9,21 @@ class CircleIterator:
         self.even = len(self.people) % 2 == 0
 
     def execute(self):
+        self.loop = self.loop*2
         if self.even and self.wielder:
-            print("even + wielder")
-            self.loop = self.loop*2
             self.people = range(self.people[0], self.people[-1], self.loop)
-            self.reset_odd_or_even()
 
         elif not self.even and self.wielder:
-            print("odd + wielder")
-            self.loop = self.loop*2
             self.people = range(self.people[0], self.people[-1] + 1, self.loop)
             self.wielder = False
-            self.reset_odd_or_even()
 
         elif self.even and not self.wielder:
-            print("even + not wielder")
-            self.loop = self.loop*2
             self.people = range(self.people[1], self.people[-1] + 1, self.loop)
-            self.reset_odd_or_even()
 
         elif not self.even and not self.wielder:
-            print("odd + not wielder")
-            self.loop = self.loop*2
             self.people = range(self.people[1], self.people[-1], self.loop)
             self.wielder = True
-            self.reset_odd_or_even()
+        self.reset_odd_or_even()
 
         if len(self.people) == 1:
             return self.people[0]
